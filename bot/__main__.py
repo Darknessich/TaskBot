7332bfef64d20cc89cf0dc200ed31b3d29f8f3b4
Anton Tasakov <blakibrown@gmail.com>
2024-10-12 07:12:37 +0300

    add session midleware

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,11 +5,14 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.enums import ParseMode
 from fluentogram import TranslatorHub
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from core import Settings
 from bot.dialogs import setup_dialogs
 from bot.utils.i18n import create_translator_hub
 from bot.middlewares.i18n import TranslatorRunnerMiddleware
+from bot.middlewares.session import DbSessionMiddleware
 from bot.handlers.commands import router
 
 logging.basicConfig(
@@ -22,6 +25,12 @@ logger = logging.getLogger(__name__)
 
 async def main() -> None:
     settings = Settings()
+
+    engine = create_async_engine(settings.pg_dns)
+
+    async with engine.begin() as conn:
+        await conn.execute(text("SELECT 1"))
+
     bot = Bot(
         token=settings.bot_token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
@@ -33,6 +42,8 @@ async def main() -> None:
     dp.include_router(router)
     setup_dialogs(dp)
 
+    Sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    dp.update.outer_middleware(DbSessionMiddleware(Sessionmaker))
     dp.update.middleware(TranslatorRunnerMiddleware())
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, _translator_hub=translator_hub)
